e651891853409d7149bed00d2e7e5a66cb03a44b
Jin Park <jinpark1504@gmail.com>
2021-12-13 00:02:49 -0500

    react, firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBoYtzFEnqCVDFt2nVL4XogREzTrJNfzQM',
+  authDomain: 'twitter-clone-4cec7.firebaseapp.com',
+  projectId: 'twitter-clone-4cec7',
+  storageBucket: 'twitter-clone-4cec7.appspot.com',
+  messagingSenderId: '96834562095',
+  appId: '1:96834562095:web:cd68935da6ccf531903eda',
+};
+
+export default initializeApp(firebaseConfig);
