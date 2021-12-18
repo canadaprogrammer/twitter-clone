86ab01f14a38043878bfe7a9b6955c66428c86a2
Jin Park <jinpark1504@gmail.com>
2021-12-17 19:25:04 -0500

    fixed untracked files

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const db = getFirestore();
+  const [ctwitt, setCtwitt] = useState('');
+  const onSubmit = async (evt) => {
+    evt.preventDefault();
+  };
+  const onChange = (evt) => {
+    const {
+      target: { value },
+    } = evt;
+    setCtwitt(value);
+  };
+  return (
+    <div>
+      <form>
+        <input
+          value={ctwitt}
+          onChange={onChange}
+          type='text'
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type='submit' value='Cloning Twitter' onClick={onSubmit} />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
